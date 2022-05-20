d233e6d645640a0df651bced40f494e8a072e12f
Romalyy <1303roma01@gmail.com>
2022-05-20 20:30:04 +0300

    Правки(2,8) завдання

--- js/task-08.js
+++ js/task-08.js
@@ -5,31 +5,41 @@ const formInputs = document.querySelectorAll('input');
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-        event.preventDefault();
+    event.preventDefault();
         
-        const formElements = event.currentTarget.elements;
+    const formElements = event.currentTarget.elements;
 
-        const emailInput = formElements.email.value;
-        const passwordInput = formElements.password.value;
-        
-        const formData = {
-                emailInput,
-                passwordInput,
-        }
-        
-        console.log(formData);
-        console.log(emailInput, passwordInput);
+    const emailInput = formElements.email.value;
+    const passwordInput = formElements.password.value;
 
-        const formInputsValue = formInputs.forEach(function (input) {
-                if (input.value === '') {
-                        alert('Все поля должны быть заполнены');    
-                }
-                
-                
-        })  
-        if (formInputsValue !== '') {
-                form.reset();
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            alert('Все поля должны быть заполнены');    
+                }  
+        }) 
+
+        if (emailInput !== '' && passwordInput !== '') {
+            form.reset();
+        console.log('email:', emailInput);
+        console.log('password:', passwordInput);
         }
 }
 
+// const form = document.querySelector('.login-form');
+// const formInputs = document.querySelectorAll('input');
 
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+//   const emailInput = formElements.email.value;
+//   const passwordlInput = formElements.password.value;
+        
+//   if (emailInput === '' || passwordlInput === '') {
+//     return alert('Все поля должны быть заполнены');
+//   }
+//   console.log('email:', emailInput);
+//   console.log('password:', passwordlInput);
+//   form.reset();
+// }
